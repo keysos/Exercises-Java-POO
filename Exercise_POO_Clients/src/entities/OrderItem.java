ec401e2a40a263e46e65a7cc8eb5397be887f7af
Gustavo Abreu <gustavoxabreux@gmail.com>
2024-04-30 16:31:45 -0300

    Exercicse Client Database Done

---
+++ Exercise_POO_Clients/src/entities/OrderItem.java
@@ -0,0 +1,47 @@
+package entities;
+
+public class OrderItem {
+
+	public Integer quantity;
+	public Double price;
+	public Product product;
+
+	public OrderItem() {
+
+	}
+
+	public OrderItem(Integer quantity, Double price, Product product) {
+		this.quantity = quantity;
+		this.price = price;
+		this.product = product;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public Double subTotal() {
+		return quantity * price;
+	}
+
+}
